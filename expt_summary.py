157c453ad4a2cf08a368541ccc25e22c193955c7
estwiz <estifanos9a@gmail.com>
2025-03-14 18:59:50 +0000

    Update experiment script

--- expt_summary.py
+++ expt_summary.py
@@ -177,8 +177,8 @@ def main():
     patterns = [
         "results/Mistral-7B-Instruct-v0.2_INF*.tsv",
         "results/Mistral-7B-Instruct-v0.2_10*.tsv",
-        # "results/gemma-7B-Instruct-v0.2_INF*.tsv",
-        # "results/gemma-7B-Instruct-v0.2_10*.tsv",
+        "results/gemma-2b-it_INF*.tsv",
+        # "results/gemma-2b-it_10*.tsv",
         # "results/Llama-3.1-8b_INF*.tsv",
         # "results/Llama-3.1-8b_10*.tsv",
     ]
---
+++ run.sh
@@ -0,0 +1,4 @@
+nohup python3 craft_adv_examples.py --split=1  > expt_1.log 2>&1
+nohup python3 craft_adv_examples.py --split=2  > expt_2.log 2>&1
+nohup python3 craft_adv_examples.py --split=3  > expt_3.log 2>&1
+nohup python3 craft_adv_examples.py --split=4  > expt_4.log 2>&1
